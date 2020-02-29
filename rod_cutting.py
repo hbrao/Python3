8ce7e08dfeacd3b6ebea8b1a12259a10333b3145
hanu.16aug <hanu.16aug@outlook.com>
2020-02-29 02:17:40 +0000

    Dynamic programming technique

--- rod_cutting.py
+++ rod_cutting.py
@@ -1,12 +1,21 @@
 p = [2, 8, 10, 12, 16, 18, 20, 25]
+r = [None] * len(p)
 
-def max_price(n):
-    if n == 0:
-        return p[0]
+def max_price(cur_len):
+    if cur_len == 1:
+        r[0] = p[0]
+        return r[0]
     else:
-        cur_max = p[n-1]
-        for i in range(n):
-            cur_max = max(cur_max, p[i] + max_price(n-i-1))
+        cur_max = p[cur_len-1]
+        for i in range(cur_len - 1):
+            new_len = cur_len - ( i+1 )
+            if r[new_len - 1] is None:
+                cur_max = max(cur_max, p[i] + max_price(new_len))
+            else:
+                cur_max = max(cur_max, p[i] + r[new_len - 1])
+        r[cur_len - 1] = cur_max
         return cur_max
 
-print(max_price(5))
+print(max_price(8))
+print(p)
+print(r)
