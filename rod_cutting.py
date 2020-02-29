8c2439d7b18ec675261160ae4134eca0d81cec84
hanu.16aug <hanu.16aug@outlook.com>
2020-02-29 01:54:37 +0000

    Recursive implementation

--- rod_cutting.py
+++ rod_cutting.py
@@ -1,1 +1,12 @@
-p = []
+p = [2, 8, 10, 12, 16, 18, 20, 25]
+
+def max_price(n):
+    if n == 0:
+        return p[0]
+    else:
+        cur_max = p[n-1]
+        for i in range(n):
+            cur_max = max(cur_max, p[i] + max_price(n-i-1))
+        return cur_max
+
+print(max_price(5))
