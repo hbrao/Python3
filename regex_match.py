877156225c4e9d157427acc86f22460c8a287cdc
hanu.16aug <hanu.16aug@outlook.com>
2020-03-02 01:07:42 +0000

    Support * operator

--- regex_match.py
+++ regex_match.py
@@ -2,6 +2,10 @@ def match(p, t):
     if not bool(p):
         return not bool(t)
     ans = bool(t) and p[0] in [t[0], '.']
-    return ans and match(p[1:], t[1:])
+    if len(p) > 1 and  p[1] == '*':
+        return ans and ( match(p, t[1:]) or match(p[2:], t[1:]) )
+    else:
+        return ans and match(p[1:], t[1:])
 
 print(match('m.ss.ssi..', 'mississipi'))
+print(match('m.s*.s*i.*', 'mississipi'))
