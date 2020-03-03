d513dd72be08b7b95d68340e68f1c91e177e0a38
Hanu <hbrao@Hanus-MacBook-Pro.local>
2020-03-03 00:15:13 -0800

    Fix

--- regex_match.py
+++ regex_match.py
@@ -1,11 +1,11 @@
-def match(p, t):
-    if not bool(p):
-        return not bool(t)
-    ans = bool(t) and p[0] in [t[0], '.']
-    if len(p) > 1 and  p[1] == '*':
-        return ans and ( match(p, t[1:]) or match(p[2:], t[1:]) )
-    else:
-        return ans and match(p[1:], t[1:])
+class Solution:
+    def isMatch(self, t: str, p: str) -> bool:
+        if not p:
+            return not t
+        ans = bool(t) and p[0] in [t[0], '.']
+        if len(p) > 1 and p[1] == '*':
+            return self.isMatch(t, p[2:]) or (ans and self.isMatch(t[1:], p))
+        else:
+            return ans and self.isMatch(t[1:], p[1:])
 
-print(match('m.ss.ssi..', 'mississipi'))
-print(match('m.s*.s*i.*', 'mississipi'))
+print(Solution().isMatch('mississipi', 'm.s*is*i.*'))
