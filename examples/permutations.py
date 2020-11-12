25f6287e588f30d1d4b59aac17d1196a609aa9dc
hbrao <hanu.16aug@outlook.com>
2020-11-12 10:28:21 -0800

    Update permutations.py

--- examples/permutations.py
+++ examples/permutations.py
@@ -4,8 +4,9 @@ def permutate(s):
         res.append(s)
     else:
         for i in range(len(s)):
-            for p in permutate(s[:i] + s[i+1:]):
-                res.append(s[i]+p)
+            for p in permutate(s[:i] + s[i + 1:]):
+                res.append(s[i] + p)
     return res
+
 
 print(permutate('(())'))
