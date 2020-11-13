3cacd1059230d41dacdb7fc796d7e1b4b8c948f0
hbrao <hanu.16aug@outlook.com>
2020-11-13 15:03:33 -0800

    minor changes

--- examples/array.py
+++ examples/array.py
@@ -1,17 +1,26 @@
+from copy import copy, deepcopy
+
 phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
 for i in range(4):
-   plist.pop()
+    plist.pop()
 plist.pop(0)
 
-plist.extend([plist.pop(),plist.pop()])
+plist.extend([plist.pop(), plist.pop()])
 
 plist.remove('\'')
 
-plist.insert(2,plist.pop(3))
+plist.insert(2, plist.pop(3))
 
 new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
+
+a = [1, 2, 3]
+c = a
+b = copy(a)
+a.append(4)
+print(b)
+print(c)
