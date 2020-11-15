df7eda4b9e5d54469852dfc9204b16af1290fb73
hbrao <hanu.16aug@outlook.com>
2020-11-14 16:28:46 -0800

    Create mathematics.py

---
+++ examples/mathematics.py
@@ -0,0 +1,11 @@
+import math
+
+print(divmod(129, 10))
+
+print(math.ceil(129.88))
+print(math.floor(129.88))
+
+print(math.sqrt(5))
+print(math.isqrt(5))
+
+print(str(math.pow(2, 4)) + ' vs ' + str(2 ** 4))
